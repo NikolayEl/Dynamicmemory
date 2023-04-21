35d3bf1a0ef28bbf08d699ec4674ab037dae4862
Nikolay Elshin <nikolay19830223@gmail.com>
2023-04-21 22:39:40 +0800

    buffer, nullptr

--- main.cpp
+++ main.cpp
@@ -13,7 +13,22 @@ void main()
 	int* arr = new int[n];
 	FillRand(arr, n);
 	Print(arr, n);
+
+	int value;
+	cout << "Введите добавляемый элемент: "; cin >> value;
+	n++;
+	int* buffer = new int[n];
+	for (int i = 0; i < n - 1; i++)
+	{
+		buffer[i] = arr[i];
+	}
 	delete[] arr;
+	arr = buffer;
+	buffer = nullptr;
+	arr[n - 1] = value;
+	Print(arr, n);
+	
+	delete[] arr, buffer;
 }
 
 void FillRand(int arr[], const int n)
